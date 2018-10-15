d22133f78b1e26c64ba2185ac9688be4322fd9b9
Taco Petri <petri.taco@gmail.com>
2018-10-15 19:19:59 +0200

    Added TorXakisModel class, an object-oriented parser for .txs files.

--- TorXakisDotNetAdapter.Tests/Source/CodeGenerationTest.cs
+++ TorXakisDotNetAdapter.Tests/Source/CodeGenerationTest.cs
@@ -16,58 +16,28 @@ namespace TorXakisDotNetAdapter.Tests
         [TestMethod]
         public void ParseTestModel()
         {
-            Dictionary<string, Dictionary<string, string>> actions = new Dictionary<string, Dictionary<string, string>>();
-
-            // Find all TXS models.
-            DirectoryInfo modelDirectory = new DirectoryInfo(Path.Combine(@"..\..\..\", "TorXakisDotNetAdapter", "Models"));
+            // Find all model files.
+            DirectoryInfo modelDirectory = new DirectoryInfo(Path.Combine(@"..\..\..\", "TorXakisDotNetAdapter.Models", "Models"));
             foreach (FileInfo modelFile in modelDirectory.GetFiles("*.txs", SearchOption.AllDirectories))
             {
-                string modelText = File.ReadAllText(modelFile.FullName);
+                // Create model from file.
+                TorXakisModel model = new TorXakisModel(modelFile);
+                Console.WriteLine(model);
 
-                // Find all TYPEDEF sections.
-                MatchCollection typedefMatches = Regex.Matches(modelText, @"TYPEDEF(.*?)ENDDEF", RegexOptions.Singleline);
-                foreach (Match typedefMatch in typedefMatches)
+                // Parse actions from model.
+                Dictionary<string, Dictionary<string, string>> actions = model.ParseActions();
+                foreach (KeyValuePair<string, Dictionary<string, string>> kvp1 in actions)
                 {
-                    string typedefText = typedefMatch.Groups[1].ToString();
-                    typedefText = typedefText.Substring(typedefText.IndexOf("::=") + "::=".Length);
-
-                    // Find all ACTION types.
-                    foreach (string typeMatch in typedefText.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries))
+                    string info = kvp1.Key;
+                    foreach (KeyValuePair<string, string> kvp2 in kvp1.Value)
                     {
-                        string typeStripped = typeMatch.Replace("\n", "").Replace("\t", "").Replace(" ", "");
-
-                        string[] typeParts = typeStripped.Split('{');
-                        string typeName = typeParts[0];
-
-                        actions.Add(typeName, new Dictionary<string, string>());
-
-                        if (typeParts.Length > 1)
-                        {
-                            string data = typeParts[1].TrimEnd('}');
-                            foreach (string paramMatch in data.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
-                            {
-                                string[] paramParts = paramMatch.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-                                string[] paramNames = paramParts[0].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                                string paramType = paramParts[1];
-                                foreach (string paramName in paramNames)
-                                {
-                                    actions[typeName].Add(paramName, paramType);
-                                }
-                            }
-                        }
+                        info += "\n    " + kvp2.Key + " " + kvp2.Value;
                     }
+                    Console.WriteLine(info);
                 }
             }
 
-            foreach (KeyValuePair<string, Dictionary<string, string>> kvp1 in actions)
-            {
-                string info = kvp1.Key;
-                foreach (KeyValuePair<string, string> kvp2 in kvp1.Value)
-                {
-                    info += "\n    " + kvp2.Key + " " + kvp2.Value;
-                }
-                Console.WriteLine(info);
-            }
+
         }
     }
 }
---
+++ TorXakisDotNetAdapter/Source/TorXakisModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TorXakisDotNetAdapter
+{
+    /// <summary>
+    /// Provides an object-oriented parser for TorXakis models (.txs files).
+    /// </summary>
+    public sealed class TorXakisModel
+    {
+        #region Definitions
+
+        /// <summary>
+        /// The file extension for model files.
+        /// </summary>
+        public const string FileExtension = ".txs";
+
+        #endregion
+        #region Variables & Properties
+
+        /// <summary>
+        /// The model file.
+        /// </summary>
+        public FileInfo File { get; private set; }
+
+        #endregion
+        #region Create & Destroy
+
+        /// <summary>
+        /// Constructor, with parameters.
+        /// </summary>
+        public TorXakisModel(FileInfo file)
+        {
+            // Sanity checks.
+            if (file == null || !file.Exists || file.Extension.ToLowerInvariant() != FileExtension)
+                throw new ArgumentException("Invalid file: " + file, nameof(file));
+
+            File = file;
+        }
+
+        /// <summary><see cref="Object.ToString"/></summary>
+        public override string ToString()
+        {
+            return GetType().Name + " " + nameof(File) + " (" + File + ")";
+        }
+
+        #endregion
+        #region Functionality
+
+        /// <summary>
+        /// Parses the defined <see cref="TorXakisAction"/> types from the model.
+        /// </summary>
+        public Dictionary<string, Dictionary<string, string>> ParseActions()
+        {
+            Dictionary<string, Dictionary<string, string>> actions = new Dictionary<string, Dictionary<string, string>>();
+
+            string modelText = System.IO.File.ReadAllText(File.FullName);
+
+            // Find all TYPEDEF sections.
+            MatchCollection typedefMatches = Regex.Matches(modelText, @"TYPEDEF(.*?)ENDDEF", RegexOptions.Singleline);
+            foreach (Match typedefMatch in typedefMatches)
+            {
+                string typedefText = typedefMatch.Groups[1].ToString();
+                typedefText = typedefText.Substring(typedefText.IndexOf("::=") + "::=".Length);
+
+                // Find all ACTION types.
+                foreach (string typeMatch in typedefText.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string typeStripped = typeMatch.Replace("\n", "").Replace("\t", "").Replace(" ", "");
+
+                    // Parse NAME.
+                    string[] typeParts = typeStripped.Split('{');
+                    string typeName = typeParts[0];
+                    actions.Add(typeName, new Dictionary<string, string>());
+
+                    // Parse PARAMETER NAMES, and their TYPES.
+                    if (typeParts.Length > 1)
+                    {
+                        string data = typeParts[1].TrimEnd('}');
+                        foreach (string paramMatch in data.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                        {
+                            string[] paramParts = paramMatch.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                            string[] paramNames = paramParts[0].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                            string paramType = paramParts[1];
+                            foreach (string paramName in paramNames)
+                            {
+                                actions[typeName].Add(paramName, paramType);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return actions;
+        }
+
+        #endregion
+    }
+}
