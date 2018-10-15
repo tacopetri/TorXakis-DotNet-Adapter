4e6d9a5f6612182ffc132f95d36631b3c51e5004
Taco Petri <petri.taco@gmail.com>
2018-10-15 22:25:01 +0200

    Added ISystemAction interface.

---
+++ TorXakisDotNetAdapter/Source/ISystemAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TorXakisDotNetAdapter
+{
+    /// <summary>
+    /// The interface for all system actions.
+    /// The classes that implement this are messages native to the system itself.
+    /// </summary>
+    public interface ISystemAction
+    {
+        #region Definitions
+
+        // TODO: Implement!
+
+        #endregion
+        #region Variables & Properties
+
+        // TODO: Implement!
+
+        #endregion
+        #region Functionality
+
+        // TODO: Implement!
+
+        #endregion
+    }
+}
