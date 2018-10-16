01c685fd57ba57c5d350b95a1f56d3d12a91cbb3
Taco Petri <petri.taco@gmail.com>
2018-10-16 19:17:08 +0200

    Added simple logging framework to get messages from the library components to the SUT. I did not want to add a dependency on log4net for example.

---
+++ TorXakisDotNetAdapter/Source/Logging/Log.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorXakisDotNetAdapter.Logging
+{
+    /// <summary>
+    /// Static class used to route all log messages.
+    /// </summary>
+    public static class Log
+    {
+        /// <summary>
+        /// Signals a new <see cref="LogMessage"/>.
+        /// </summary>
+        public static event Action<LogMessage> Message;
+
+        /// <summary>
+        /// Invokes the <see cref="Message"/> event.
+        /// </summary>
+        private static void OnMessage(object sender, LogLevel level, string message)
+        {
+            LogMessage log = new LogMessage(sender, level, message);
+            Message?.Invoke(log);
+        }
+
+        /// <summary>
+        /// Sends the given <see cref="LogLevel.Debug"/> message.
+        /// </summary>
+        public static void Debug(object sender, string message)
+        {
+            OnMessage(sender, LogLevel.Debug, message);
+        }
+
+        /// <summary>
+        /// Sends the given <see cref="LogLevel.Info"/> message.
+        /// </summary>
+        public static void Info(object sender, string message)
+        {
+            OnMessage(sender, LogLevel.Info, message);
+        }
+
+        /// <summary>
+        /// Sends the given <see cref="LogLevel.Warn"/> message.
+        /// </summary>
+        public static void Warn(object sender, string message)
+        {
+            OnMessage(sender, LogLevel.Warn, message);
+        }
+
+        /// <summary>
+        /// Sends the given <see cref="LogLevel.Error"/> message.
+        /// </summary>
+        public static void Error(object sender, string message)
+        {
+            OnMessage(sender, LogLevel.Error, message);
+        }
+    }
+}
---
+++ TorXakisDotNetAdapter/Source/Logging/LogMessage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TorXakisDotNetAdapter.Logging
+{
+    /// <summary>
+    /// Represents a simple <see cref="Log"/> message.
+    /// Contains <see cref="UTC"/>, <see cref="Level"/> and <see cref="Message"/>.
+    /// </summary>
+    public sealed class LogMessage
+    {
+        #region Definitions
+
+        // TODO: Implement!
+
+        #endregion
+        #region Variables & Properties
+
+        /// <summary>
+        /// The sender object.
+        /// </summary>
+        public object Sender { get; private set; }
+
+        /// <summary>
+        /// The Coordinated Universal Time (UTC).
+        /// </summary>
+        public DateTime UTC { get; private set; }
+
+        /// <summary>
+        /// The <see cref="LogLevel"/> value.
+        /// </summary>
+        public LogLevel Level { get; private set; }
+
+        /// <summary>
+        /// The user-friendly message.
+        /// </summary>
+        public string Message { get; private set; }
+
+        #endregion
+        #region Create & Destroy
+
+        /// <summary>
+        /// Constructor, with parameters.
+        /// </summary>
+        public LogMessage(object sender, LogLevel level, string message)
+        {
+            Sender = sender;
+            UTC = DateTime.UtcNow;
+            Level = level;
+            Message = message;
+        }
+
+        /// <summary><see cref="Object.ToString"/></summary>
+        public override string ToString()
+        {
+            return "[" + UTC.ToLocalTime().ToString("HH:mm:ss.fff") + "] "
+                + "[" + Sender.GetType().Name + "] "
+                + "[" + Level.ToString().ToUpperInvariant() + "] "
+                + Message;
+        }
+
+        #endregion
+    }
+}
