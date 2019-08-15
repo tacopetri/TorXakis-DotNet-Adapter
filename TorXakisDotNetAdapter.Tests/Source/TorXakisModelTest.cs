c2e9b3933bfac9cd0b2d59072509d2a974cfada2
Taco Petri <petri.taco@gmail.com>
2019-08-15 20:12:50 +0200

    Deleted legacy code using the Microsoft.Solver.Foundation framework.

--- TorXakisDotNetAdapter.Tests/Source/TorXakisModelTest.cs
+++ TorXakisDotNetAdapter.Tests/Source/TorXakisModelTest.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.SolverFoundation.Common;
-using Microsoft.SolverFoundation.Services;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.IO;
